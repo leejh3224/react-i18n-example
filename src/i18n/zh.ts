639f67ffb34d8f4f02ff14cc1a61b1b9c7eb9f2c
leejh3224 <leejh3224@naver.com>
2018-04-18 02:17:05 +0900

    add zh dict

---
+++ src/i18n/zh.ts
@@ -0,0 +1,14 @@
+import { Dictionary } from './Dictionary'
+
+const zh: Dictionary = {
+  address: '在韩国的某个地方',
+  emailAddress: 'leejh3224@naver.com',
+  getInTouch: 'I18n示例应用程序',
+  getInTouchBody:
+    '我们会鼓励你发挥程序员的三大美德：懒惰，不耐烦和傲慢。--Larry Wall',
+  languageName: '汉语',
+  phoneNumber: '(82) 10-xxxx-xxxx',
+  selectLanguage: '选择一种语言',
+}
+
+export default zh
